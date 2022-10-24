2e9c3433d85434ba37d55dc283f5a8aad207a05d
Vladyslav Liashuk <mystikhawk333@gmail.com>
2022-10-24 12:25:37 +0300

    добавлено движение формы добавления задачи

--- DailyProg/wwwroot/js/site.js
+++ DailyProg/wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(".dailyBackground .block-tables .block-daily-tasks .table .block-add .button-add").click(function () {
+    $(".add-task-background").addClass("active");
+    $(".add-daily-form").addClass("active");
+});
+$(".dailyBackground .block-tables .block-everyday-tasks .table .block-add .button-add").click(function () {
+    $(".add-task-background").addClass("active");
+    $(".add-everyday-form").addClass("active");
+});
+$(".dailyBackground .block-tables .block-noterms-tasks .table .block-add .button-add").click(function () {
+    $(".add-task-background").addClass("active");
+    $(".add-noterms-form").addClass("active");
+});
+$(".add-task-background .add-daily-form .block-close .button-close").click(function () {
+    $(".add-task-background").removeClass("active");
+    $(".add-daily-form").removeClass("active");
+});
+$(".add-task-background .add-everyday-form .block-close .button-close").click(function () {
+    $(".add-task-background").removeClass("active");
+    $(".add-everyday-form").removeClass("active");
+});
+$(".add-task-background .add-noterms-form .block-close .button-close").click(function () {
+    $(".add-task-background").removeClass("active");
+    $(".add-noterms-form").removeClass("active");
+});
